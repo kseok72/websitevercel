50b2ded80ae2c687daf8cdb5ad0dcd357c6a8e2a
kseok72 <84307557+kseok72@users.noreply.github.com>
2026-06-10 02:15:41 +0900

    Update layout.tsx
    내용수정,

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "수험 쉼표",
-  description: "익명으로 수험생활 팁과 고민을 남기는 안전한 공간",
+  title: "수험 생활 도움 사이트",
+  description: "수험생활 팁을 배우고 익명으로 고민을 남기는 공간",
 };
 
 export const viewport: Viewport = {
